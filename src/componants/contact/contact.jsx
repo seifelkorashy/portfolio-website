a7c2788043169cfbce2c9e1be9e76b7f698209c7
seif elkorashy <s.elkorashy74@gmail.com>
2025-02-01 22:37:57 +0200

    add dark & light mode, add send message to gamil & finish the project

--- src/componants/contact/contact.jsx
+++ src/componants/contact/contact.jsx
@@ -1,8 +1,11 @@
 import "./contact.css";
 import Lottie from "lottie-react";
 import contactAnimation from "../../animation/contact.json";
+import doneAnimation from "../../animation/done.json";
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function Contact() {
+    const [state, handleSubmit] = useForm("xgvoyzyv");
     return(
         <section className="contact">
             <h1 className="title">
@@ -12,12 +15,39 @@ export default function Contact() {
             <p>Contact us for more information and Get notified when I publish something new.</p>
 
             <div className="flex">
-                <form className="flex">
-                    <input type="text" placeholder="Your Name" required />
-                    <input type="email" placeholder="Your Email" required />
-                    <textarea placeholder="Your Message" required></textarea>
-                    <button type="submit">Send</button>
+                <form  onSubmit={handleSubmit} className="flex">
+                    <input type="text" name="name" placeholder="Your Name" required />
+                    <ValidationError 
+                        prefix="Email" 
+                        field="name"
+                        errors={state.errors}
+                    />
+                    <input type="email" name="email" placeholder="Your Email" required />
+                    <ValidationError 
+                        prefix="Email" 
+                        field="email"
+                        errors={state.errors}
+                    />
+                    <textarea name="message" placeholder="Your Message" required></textarea>
+                    <ValidationError 
+                        prefix="Email" 
+                        field="message"
+                        errors={state.errors}
+                    />
+                    <button type="submit" disabled={state.submitting}>
+                        {state.submitting ? "Sending..." : "Send"}
+                    </button>
+                    {state.succeeded && 
+                    
+                    <div className="flex" style={{marginRight:"3rem"}}>
+                        <Lottie animationData={doneAnimation}
+                        style={{height:"80px"}} loop={false}/>
+                        <p style={{margin:"-2rem"}}>Thanks for contacting us!</p>
+                    </div>
+
+                    }
                 </form>
+
                 <div className="animation">
                     <Lottie animationData={contactAnimation}
                     style={{height:"300px"}}/>
--- src/componants/header/header.jsx
+++ src/componants/header/header.jsx
@@ -1,8 +1,19 @@
 import "./header.css";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 export default function Header() {
-    const [showModel , setShowModel] = useState(false)
+    const [showModel , setShowModel] = useState(false);
+    const [theme, setTheme] = useState(localStorage.getItem("theme"));
+
+    useEffect(() => {
+        if (theme === "dark") {
+            document.body.classList.add("dark");
+            document.body.classList.remove("light");
+        }else {
+            document.body.classList.add("light");
+            document.body.classList.remove("dark");
+        }
+    }, [theme]);
     return (
         <header className="flex">
             <img src="public/me-modified.png" alt="" style={{width:"45px"}} />
@@ -16,13 +27,21 @@ export default function Header() {
                 </ul>
             </nav>
 
-            <button style={{all:"unset"}}>
-                <span className="icon-moon-o moon"></span>
+            <button style={{all:"unset"}} onClick=
+            {() => {
+                localStorage.setItem("theme", theme === "dark" ? "light" : "dark");
+                setTheme(localStorage.getItem("theme"));
+            }
+            }
+            >
+                <span className={theme === "light" ? "icon-moon-o moon" : "icon-sun sun"}></span>
             </button>
+
+
         {showModel && (
                 <div className="fixed">
                 <ul className="model">
-                <li style={{textAlign:"right", border:"none"}}><button style={{backgroundColor:"black"}} onClick={() => {setShowModel(false)} } className="close icon-close"></button></li>
+                <li style={{textAlign:"right", border:"none"}}><button onClick={() => {setShowModel(false)} } className="close icon-close"></button></li>
                     <li><a href="">about</a></li>
                     <li><a href="">articals</a></li>
                     <li><a href="">projects</a></li>
